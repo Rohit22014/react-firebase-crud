aaa9c7adfa482be7d6e72db0f56f9877b73f1010
Rohit22014 <44193384+Rohit22014@users.noreply.github.com>
2022-04-11 23:32:34 +0100

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,7 @@ import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from '@firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAWtLPI0HDlQjGgZt3Y2cjXGDnV-Ki1n7Y",
-    authDomain: "react-firebase-crud-cad52.firebaseapp.com",
-    projectId: "react-firebase-crud-cad52",
-    storageBucket: "react-firebase-crud-cad52.appspot.com",
-    messagingSenderId: "1065699089752",
-    appId: "1:1065699089752:web:b0f395fd3eed4a2e07592d",
-    measurementId: "G-1PCV5SYPY3"
+   
 };
 
 const app = initializeApp(firebaseConfig);
